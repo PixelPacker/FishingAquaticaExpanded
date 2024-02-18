c18e4497b5aa37ef5df3b7562c4b085f79b9299f
Kevin Stoffan <PixelPacker1@gmail.com>
2024-02-17 21:05:29 -0500

    Cleanup - Removed unused classes - Optimized imports

--- src/main/java/net/pixelpacker/crates/blocks/BasicCrateBlock.java
+++
@@ -1,7 +0,0 @@
-package net.pixelpacker.crates.blocks;
-
-public class BasicCrateBlock extends CrateBlock {
-    public BasicCrateBlock(Settings settings) {
-        super(settings);
-    }
-}
--- src/main/java/net/pixelpacker/registers/BlockReg.java
+++ src/main/java/net/pixelpacker/registers/BlockReg.java
@@ -3,16 +3,14 @@ package net.pixelpacker.registers;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.pixelpacker.FishingAquaticaExpanded;
-import net.pixelpacker.crates.blocks.BasicCrateBlock;
-import net.minecraft.block.Block;
 import net.pixelpacker.crates.blocks.MinerCrate;
-import net.pixelpacker.crates.blocks.SpicyCrateBlock;
 
 public class BlockReg {
     public static final Block MINER_CRATE = new MinerCrate(FabricBlockSettings.create().strength(2.0f).sounds(BlockSoundGroup.WOOD));
